785d5d6d1cff151df280aeb28106ec6f44a4c451
Rasmus Andersen <rasmus@growupstudio.dk>
2022-05-06 15:42:49 +0200

    longer text

--- components/Navbar.js
+++ components/Navbar.js
@@ -23,7 +23,10 @@ export default function Navbar() {
       <nav className={scss.navbar}>
         <div className={`${scss.drawer} ${ openContact == true ? `${scss.opened}` : `${scss.closed}`}`}>
           <div className={scss.menu}>
-            <h1>Jeg har 20 års erfaring fra kommunikations- og reklameverdenen. Jeg har især arbejdet meget med store retailere, hvor jeg har løst alt fra store kampagner og årsoplæg til mindre grafiske opgaver.</h1>
+            <h1>Jeg har 20 års erfaring fra kommunikations- og reklameverdenen. Jeg har især arbejdet meget med store retailere, hvor jeg har løst alt fra store kampagner og årsoplæg til mindre grafiske opgaver.<br/><br/>
+            Jeg er vant til at arrangere større og mindre fotoshoots/filmoptagelser med mennesker og med produkter. Jeg kan styre slagets gang både i planlægningen og i udførelsen af og ved hvad der skal til for at komme frem til et vellykket resultat.<br/><br/>
+            Jeg elsker at udvikle og skabe nye universer til mine kunder. Det kan være forslag til ny billede stil, konceptudvikling, kampagneudvikling eller styling af div. produkter/stilleben. Både dem med pincet og dem med store baggrunde og møbler.<br/><br/>
+            Jeg er altid åben for en snak omkring den bedste og mest kreative måde at løse en problemstilling på.</h1>
             <div className={scss.contacts}>
               <Link href='tel:+4540558860'><a>+45 40 55 88 60</a></Link>
               <Link href='mailto: liv@cramerholme.dk'><a>liv@cramerholme.dk</a></Link>
